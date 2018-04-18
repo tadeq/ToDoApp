325336f0dd4f945daeba943b514af2a8b9c0f01f
Maciej Moskal <33002299+tadeq@users.noreply.github.com>
2018-04-18 22:35:23 +0200

    inputStream path corrected

--- ToDo/src/agh/po/projekt/ToDoApp.java
+++ ToDo/src/agh/po/projekt/ToDoApp.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class ToDoApp {
     public static void main (String args[]) {
         AppRunner application=new AppRunner();
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("E://ToDo/ToDoApp.bin"))){
+        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream("ToDoApp.bin"))){
             application=(AppRunner)inputStream.readObject();
         } catch(FileNotFoundException e){
             System.out.println(e.getMessage());
